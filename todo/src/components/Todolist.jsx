306950e61f12c0af0da56dcd7b2076dca57b69bf
kalebbirhanu <birhanukaleb62@gmail.com>
2024-02-12 15:16:39 +0300

    reinitialized files

--- todo/src/components/Todolist.jsx
+++ todo/src/components/Todolist.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap/Button';
+
 
 
 
@@ -9,31 +9,30 @@ const Todolist = () => {
   const[input, SetInput] = useState('')
 
   const addTask = () => {
-    if(tasks.trim() !== '') {
+    
       const update = ([...tasks, input])
       SetTask(update);
       SetInput('');
-
-    }
+   
 }
 
 const removeTask = (index) => {
-  if(tasks.trim !== '') {
+
 
     const update = tasks.filter((_, i) => i !== index);
     SetTask(update);
-  }
+ 
   }
   return (
     <>
-      <h1>Todolist</h1>
+      <h1 className="blue">Todolist</h1>
       <input type="text" value={input} onChange={(e) => SetInput(e.target.value)} autoFocus/>
-      <Button type="submit"onClick={addTask} >add</Button>
+      <button type="submit"onClick={addTask} >add</button>
     <div>
       {tasks.length === 0 ? (
         <p>there is no task available</p>
       ) : ( <ul>
-        {tasks.map((task, index) => ( <li key={index}>{task}<Button type="submit" onClick={() => removeTask(index)}>remove</Button> </li>
+        {tasks.map((task, index) => ( <li key={index}>{task}<button type="submit" onClick={() => removeTask(index)}>remove</button> </li>
       ))}
       </ul> )}
       
